9ade2431961359dfa5b8e654a1c1a033de51e392
suhunlin <lin.suhun@msa.hinet.net>
2023-12-19 11:50:21 +0800

    init video view

--- app/src/main/java/com/suhun/pip/MainActivity.java
+++ app/src/main/java/com/suhun/pip/MainActivity.java
@@ -3,12 +3,20 @@ package com.suhun.pip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        videoView = findViewById(R.id.lid_videoView);
     }
 }
